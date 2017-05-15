ea495ad4c531d5ea0982d4223a599ffc31531c00
Jesse Schmucker <jesselee34@gmail.com>
2017-05-15 12:11:03 -0500

    More progress on the refactor

--- server/routes/add/index.js
+++ server/routes/add/index.js
@@ -6,6 +6,35 @@ const controller = require('./controller');
 const router = express.Router();
 
 // Handle GET requests
-router.get('/', controller);
+router.get('/', (res, req) => {
+  let response;
+  let status;
+  let args = res.query.operands;
+  try {
+    // Success
+    response = controller(args);
+  } catch (err) {
+    // Failure
+    if (err.code === '400-1' || err.code === '400-2') {
+      res.status(400);
+
+      response = {
+        status: err.status,
+        message: err.message
+      };
+    } else {
+      res.status(500);
+
+      respons = {
+        status: 'Internal Server Error',
+        message: 'There was an unkown internal server error. Please try again later.'
+      };
+    }
+
+    return;
+  }
+
+  res.json(response);
+});
 
 module.exports = router;
